0902a6b1babb9c9795fe20e2a716bc5d5044cd93
Joseph Harry <findarato@gmail.com>
2023-06-15 15:30:09 -0500

    :hammer: Refactor(podcasts): Getting ready for book player support - :hammer: Podcasts are now separated and included when needed - :sparkle: New environment variables for books - :sparkle: Dockerfile support passing environment variables Signed-off-by: Joseph Harry <findarato@gmail.com>

---
+++ podcast_player.py
@@ -0,0 +1,102 @@
+import json
+import os
+import random
+import time
+import opml
+import pychromecast
+
+import logging
+
+from datetime import datetime
+
+from player import Player
+
+from update_feeds import Update_feeds
+
+#
+# Update the list of podcasts from a feed
+#
+class Podcast_player():
+
+    def __init__(self, cast_name, TPPTP=0):
+        self.chrome_cast = cast_name
+        self.total_podcasts_to_play = TPPTP
+        self.podcasts = []
+        self.json_data = []
+
+    def getUpdate(self):
+
+        logging.info(f'Updating podcast list')
+
+        outline = opml.parse("podcasts.xml")
+
+        logging.info("Getting Feeds")
+        # try:
+        if outline[0].text == "feeds":  # This is a pocket casts feed
+            for podcast_feed in outline[0]:
+                self.json_data.extend(Update_feeds.getFeeds(podcast_feed.xmlUrl))
+        else:
+            for podcast_feed in outline:
+                self.json_data.extend(Update_feeds.getFeeds(podcast_feed.xmlUrl))
+
+        Update_feeds.writeFeeds(self.json_data)
+
+    def play(self):
+
+        # print("Writting JSON data")
+
+        timestamp = datetime.timestamp( datetime.now() )
+
+        st = os.stat('podcasts.json')
+
+        with open('podcasts.json') as json_file:
+            podcast_data = json.load(json_file)
+
+        if len(podcast_data) == 0:
+            self.getUpdate()
+            with open('podcasts.json') as json_file:
+                podcast_data = json.load(json_file)
+
+        logging.info(f'Going to play: {self.total_podcasts_to_play}')
+        logging.info(f'Chromecast: {self.chrome_cast}')
+
+        logging.info("Selecting Podcasts")
+
+        count=0
+
+
+        #
+        # Build out total list of podcasts to play
+        #
+        while len(self.podcasts) < self.total_podcasts_to_play:
+            podCast_selected = random.choice(podcast_data)
+            self.podcasts.append(podCast_selected)
+            podcast_title = podCast_selected['title']
+            logging.info(f'{count}: {podcast_title}')
+            count=count+1
+
+        chrome_cast_target, browser = pychromecast.get_listed_chromecasts(friendly_names=[self.chrome_cast])
+
+
+        if len(chrome_cast_target) == 0:
+            logging.critical(f'No Devices Found')
+            exit()
+        else:
+            logging.info(f'Looking for: {self.chrome_cast} ')
+            logging.info(f'Found it ')
+
+
+        chrome_cast_target[0].wait()
+
+        chrome_cast = chrome_cast_target[0]
+
+
+        chromecast_player = Player(chrome_cast)
+
+        chromecast_player.play(self.podcasts)
+
+
+        # book_player = Player(chrome_cast)
+        # book_player.play(BOOK,100)
+
+        browser.stop_discovery()
